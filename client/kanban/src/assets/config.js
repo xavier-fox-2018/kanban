92e6fda5831f7a1bc5ed531a8c56bc2e170679c6
Yaya Hidayatullah <yayahidayatullah@rocketmail.com>
2018-11-20 23:00:40 +0700

    done

--- client/kanban/src/assets/config.js
+++ client/kanban/src/assets/config.js
@@ -1,10 +1,5 @@
 var config = {
-    apiKey: "AIzaSyA9QrHet7qLSiMPEQg5AINNz-eF9DEMIC0",
-    authDomain: "mykanban88.firebaseapp.com",
-    databaseURL: "https://mykanban88.firebaseio.com",
-    projectId: "mykanban88",
-    storageBucket: "mykanban88.appspot.com",
-    messagingSenderId: "40327313099"
+    // ??????
   };
 firebase.initializeApp(config);
 
